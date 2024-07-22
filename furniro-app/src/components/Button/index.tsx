c992590bf8f1e54bdeb4c57dce080c630c0a058f
Denilson <denilsob.b.sousa2021@gmail.com>
2024-07-22 20:43:19 -0300

    feat: add button component
    add button component with all variants created with tailwind variants library

---
+++ furniro-app/src/components/Button/index.tsx
@@ -0,0 +1,39 @@
+import { ComponentProps, ReactNode } from "react"
+import { tv, VariantProps } from "tailwind-variants"
+
+const buttonVariants = tv({
+  base: 'flex items-center justify-center font-poppins text-[1rem] uppercase',
+
+  variants: {
+    variant: {
+      primary: 'bg-light-peach-900 text-white font-bold',
+      secondary: 'bg-light-peach-900 text-white font-semibold',
+      tertiary: 'bg-white text-light-peach-900 font-semibold',
+      outlined:
+        'border-2 border-light-peach-900 text-light-peach-900 font-semibold',
+      'outlined-secondary':
+        'border-2 border-black text-black font-normal rounded-2xl',
+    },
+
+    size: {
+      default: 'w-[222px] h-[74px]',
+      xl: 'w-[225px] h-[48px]',
+      md: 'w-[215px] h-[64px]',
+      sm: 'w-[176px] h-[48px]',
+      xs: 'w-[48px] h-[48px]',
+    },
+  },
+});
+
+type ButtonProps = {
+  children: ReactNode;
+} & ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants>;
+
+export function Button({ children, variant, size, ...rest} : ButtonProps) {
+  return (
+    <button {...rest} className={buttonVariants({ variant, size })}>
+      {children}
+    </button>
+  )
+}
