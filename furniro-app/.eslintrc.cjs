486b11b28be5265c06e34668967aa53dee0b9f1c
Denilson <denilsob.b.sousa2021@gmail.com>
2024-07-22 17:20:42 -0300

    chore: add Eslint and Prettier rules
    add Eslint and Prettier to improve code quality, formatting and errors fix

--- furniro-app/.eslintrc.cjs
+++ furniro-app/.eslintrc.cjs
@@ -5,14 +5,20 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import', 'simple-import-sort', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'import/newline-after-import': ['error', { count: 1 }],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'no-empty': ['error'],
   },
-}
+};
