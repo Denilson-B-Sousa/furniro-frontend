97ff2cf832b1da1fa6c27a9bee06b46adc4209ca
Denilson <denilsob.b.sousa2021@gmail.com>
2024-07-22 21:00:33 -0300

    chore: add setup of Vitest and Testing Library React
    add librarys for tests

--- furniro-app/vite.config.ts
+++ furniro-app/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
+/// <reference types="vitest"/>
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
