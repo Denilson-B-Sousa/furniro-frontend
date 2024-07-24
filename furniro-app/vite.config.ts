240e91a83eff8d464f865d70bc8d46f2ece82906
Denilson <denilsob.b.sousa2021@gmail.com>
2024-07-24 00:24:06 -0300

    chore: add vite-tsconfig-paths
    give vite the ability to resolve imports using typescript path mapping

--- furniro-app/vite.config.ts
+++ furniro-app/vite.config.ts
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 /// <reference types="vitest"/>
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+     alias: {
+      src: "/src"
+     },
+    }
 });
