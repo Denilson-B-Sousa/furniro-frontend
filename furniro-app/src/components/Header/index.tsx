dc6e2877fca1a22d7456100c58157a5e222f1616
Denilson <denilsob.b.sousa2021@gmail.com>
2024-08-02 03:25:02 -0300

    feat: add dropdown menu to mobile devices layout
    use radix-ui/react-dropdown-menu to create the dropdown menu

--- furniro-app/src/components/Header/index.tsx
+++ furniro-app/src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { Modal } from '@components/Modal';
-import { List } from '@phosphor-icons/react';
+import { BagSimple, BookOpenText, House, List, Phone } from '@phosphor-icons/react';
 import * as Dialog from '@radix-ui/react-dialog';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { NavLink } from 'react-router-dom';
 
 import { Navbar } from '../Navbar/index';
@@ -45,14 +46,57 @@ export function Header() {
           <Modal />
         </Dialog.Root>
         <div className='lg:hidden'>
-          <button>
-            <List
-              color='black'
-              width={32}
-              height={32}
-              className='inline-flex items-end'
-            />
-          </button>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+              <List
+                color='black'
+                width={32}
+                height={32}
+                className='inline-flex items-end'
+              />
+            </DropdownMenu.Trigger>
+
+            <DropdownMenu.Portal>
+              <DropdownMenu.Content
+                className='data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade z-50 min-w-[220px] rounded-md bg-white p-8 font-poppins text-xl font-medium shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform]'
+                sideOffset={10}
+              >
+                <DropdownMenu.Item>
+                  <NavLink to={'/'} className='flex items-center gap-4 py-4'>
+                    <House width={24} height={24} />
+                    Home
+                  </NavLink>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item>
+                  <NavLink
+                    to={'/shop'}
+                    className='flex items-center gap-4 py-4'
+                  >
+                    <BagSimple width={24} height={24} />
+                    Shop
+                  </NavLink>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item>
+                  <NavLink
+                    to={'/about'}
+                    className='flex items-center gap-4 py-4'
+                  >
+                    <BookOpenText width={24} height={24} />
+                    About
+                  </NavLink>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item>
+                  <NavLink
+                    to={'/contact'}
+                    className='flex items-center gap-4 py-4'
+                  >
+                    <Phone width={24} height={24} />
+                    Contact
+                  </NavLink>
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Portal>
+          </DropdownMenu.Root>
         </div>
       </div>
     </header>
